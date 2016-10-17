402169af08b0329863ba0b16803c7029ffef1e30
tiga05 <tiga05@gmx.de>
2016-10-17 21:32:30 +0200

    - adding meteor testapplication

--- socially/client/main.js
+++ socially/client/main.js
@@ -6,15 +6,13 @@ angular.module('socially', [
     angularMeteor,
     uiRouter
 ])
-    .controller('PartiesListCtrl', ['$scope', function($scope) {
-        $scope.parties = [{
-            'name': 'Dubstep-Free Zone',
-            'description': 'Can we please just for an evening not listen to dubstep.'
-        }, {
-            'name': 'All dubstep all the time',
-            'description': 'Get it on!'
-        }, {
-            'name': 'Savage lounging',
-            'description': 'Leisure suit required. And only fiercest manners.'
-        }];
-    }]);
+
+.controller('PartiesListCtrl', function($scope) {
+    //'ngInject';
+
+    $scope.helpers({
+        parties() {
+            return Parties.find({});
+        }
+    });
+});
